3382ce1f8af71f5e33caa5941cdd0684aa35858b
boysimon10 <simondiouf2710@gmail.com>
2024-08-02 14:05:53 +0000

    match card

--- components/BodyComponent.js
+++ components/BodyComponent.js
@@ -1,24 +1,35 @@
 import { Text, View, TouchableOpacity, ScrollView, Image } from "react-native";
 import ScoreCardComponent from "./ScoreCardComponent";
+import MatchCardComponent from "./MatchCardComponent";
 
 export default function BodyComponent() {
   return (
-    <View className="mb-[175px] bg-gray-50 ">
-      <View className="mt-2 mx-4">
-        <Text className="font-bold text-xl">Derniers Match</Text>
+    <View className="mb-[175px]">
+      <View className="bg-gray-50">
+        <View className="mt-2 mx-4">
+          <Text className="font-bold text-xl">Derniers Match</Text>
+        </View>
+        <View className="">
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View className="my-4 ml-4 flex flex-row">
+              <ScoreCardComponent className="mx-2" />
+            </View>
+            <View className="my-4 mx-4 flex flex-row">
+              <ScoreCardComponent className="mx-2" />
+            </View>
+            <View className="my-4 flex flex-row">
+              <ScoreCardComponent className="mx-2" />
+            </View>
+          </ScrollView>
+        </View>
       </View>
-      <View className="">
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <View className="my-4 mx-4 flex flex-row">
-            <ScoreCardComponent className="mx-2"/>
-          </View>
-          <View className="my-4 mx-2 flex flex-row">
-            <ScoreCardComponent className="mx-2"/>
-          </View>
-          <View className="my-4 mx-2 flex flex-row">
-            <ScoreCardComponent className="mx-2"/>
-          </View>
-        </ScrollView>
+      <View className="m-4">
+        <View className="mb-4">
+          <MatchCardComponent />
+        </View>
+        <View className="mb-4">
+          <MatchCardComponent />
+        </View>
       </View>
     </View>
   );
---
+++ components/MatchCardComponent.js
@@ -0,0 +1,53 @@
+import { Text, View, TouchableOpacity, Image } from "react-native";
+import { BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/solid";
+
+export default function MatchCardComponent() {
+  return (
+    <View className="h-32 w-full bg-white rounded-2xl shadow-sm">
+      <View className="mx-4 mt-4 mb-2 flex flex-row justify-between items-center">
+        <Text className=" font-semibold">
+          Premier League • 08/08/2024
+        </Text>
+          <Text className=" text-red-500 font-semibold">90'</Text>
+      </View>
+      <View className="flex flex-row justify-center mx-4">
+        <View className="h-px bg-gray-200 w-full"></View>
+      </View>
+      <View className="flex flex-row justify-between mx-4 mt-4">
+        <View className="flex flex-row items-center">
+          <Image
+            source={{
+              uri: "https://media.api-sports.io/football/teams/38.png",
+            }}
+            className="w-6 h-6"
+            resizeMode="contain"
+          />
+          <Text className="mx-2 text-gray-400">Watford</Text>
+        </View>
+        <View className="mx-4">
+          <Text className="font-bold">2</Text>
+        </View>
+      </View>
+
+      <View className="flex flex-row justify-end">
+        <View className="h-px bg-gray-200 w-8 mx-5"></View>
+      </View>
+
+      <View className="flex flex-row justify-between mx-4 mt-2">
+        <View className="flex flex-row items-center">
+          <Image
+            source={{
+              uri: "https://media.api-sports.io/football/teams/50.png",
+            }}
+            className="w-6 h-6"
+            resizeMode="contain"
+          />
+          <Text className="mx-2 text-gray-400">Man. City</Text>
+        </View>
+        <View className="mx-4">
+          <Text className="font-bold">4</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
